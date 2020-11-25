f8416d11295ea09ebae91b23c233e4894e61b137
Leandro Gil <leangilutn@gmail.com>
2020-11-24 22:50:11 -0300

    Ejemplo tags

--- src/app/Pages/productos-alta/productos-alta.component.ts
+++ src/app/Pages/productos-alta/productos-alta.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators,FormArray } from '@angular/forms';
 import { ProductosService } from 'src/app/services/productos.service';
 import {  FileUploader } from 'ng2-file-upload';//Incluir
 import { CategoriasService } from 'src/app/services/categorias.service';
@@ -15,6 +15,7 @@ export class ProductosAltaComponent implements OnInit {
     headers: [{ name: 'x-access-token', value: localStorage.getItem("token") }], itemAlias: 'photo'});
   myForm:FormGroup;
   categorias=<any>[];
+  tags:FormArray;
   constructor(
     private fb:FormBuilder,
     private productosService:ProductosService,
@@ -26,17 +27,25 @@ export class ProductosAltaComponent implements OnInit {
       price:["",[Validators.required]],
       quantity:["",[Validators.required]],
       category:["",[Validators.required]],
-      tags:["",[Validators.required]],
+      tags:this.fb.array([ this.createItem() ]),
       images:["",[Validators.required]]
     })
-
+    
     this.categoriasService.getAll()
     .subscribe(data=>{
       console.log(data);
       this.categorias=data;
     })
   }
-  
+  createItem(): FormGroup {
+    return this.fb.group({
+      name: ['']
+    });
+  }
+  addItem(): void {
+    this.tags = this.myForm.get('tags') as FormArray;
+    this.tags.push(this.createItem());
+  }
   guardar(){
     this.uploader.uploadAll();
     this.uploader.onCompleteItem = (item:any,response:any,status:any,headers:any) =>{
